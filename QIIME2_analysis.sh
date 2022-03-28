5579756887da4b584b19050f2315ae31a106241c
JamesTCullen <100860364+JamesTCullen@users.noreply.github.com>
2022-03-28 15:54:37 +0100

    Update QIIME2_analysis.sh

--- QIIME2_analysis.sh
+++ QIIME2_analysis.sh
@@ -1,3 +1,17 @@
+#This script relates to pre-processing of demultiplexed paired-end 16S and ITS2 rDNA sequences (available at: https://www.ebi.ac.uk/ena/browser/view/PRJEB49004) 
+#for Cullen et al. (2022) in Animal - Open Space. Demultiplexed paired-end sequences were imported (in Casava 1.8 demultiplexed paired-end format) 
+#into QIIME2 v.2020.8.0 (Bolyen et al., 2019), which was installed on a virtual machine (VirtualBox 6.0). 
+#Forward and reverse reads were quality assessed using the ‘qiime demux summarize’ command, FastQC v.0.11.5 and MultiQC v.1.9. 
+#The 16S and ITS2 primers were removed from reads using the cutadapt plugin (Martin, 2011). 
+#The QIIME2 DADA2 (Callahan et al., 2016) plugin was used for filtering and dereplication, chimera removal, merging paired-end reads 
+#and to infer amplicon sequence variants (ASVs) in each sample after truncating reads to remove low quality bases. 
+#Read 1 and read 2 of the 16S rDNA sequences were truncated at 267 and 183 bp, respectively, while ITS2 sequences were truncated at 266 and 187 bp, respectively. 
+#For bacterial sequences, taxonomy was assigned to each ASV using a Naive Bayes classifier trained on 16S V3-V4 sequences from the Silva database (Version 138) 
+#with the ‘q2-feature-classifier’ plugin, while taxonomy was assigned to fungal ASVs using a Naive Bayes classifier trained on full-length ITS sequences 
+#from the UNITE v.8.3 database (Kõljalg et al., 2013).
+
+###16S sequence pre-processing###
+
 #Make a new directory to store analysis outputs
 mkdir meth-dev-16S
 
